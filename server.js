2af649ab910cefce194f1fdf4b0f22436893aff8
KenTandrian <kenricktan11@gmail.com>
2022-01-04 20:45:27 +0700

    fix db

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
