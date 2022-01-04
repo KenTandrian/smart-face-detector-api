70bd01e361bfaacb31cfe7cd03492e3b91820d2d
KenTandrian <kenricktan11@gmail.com>
2022-01-04 20:30:12 +0700

    adding database connection

--- server.js
+++ server.js
@@ -9,13 +9,22 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//         host : '127.0.0.1',
+//         port : 5432,
+//         user : 'postgres',
+//         password : 'test',
+//         database : 'smart-brain'
+//     }
+// });
+
 const db = knex({
     client: 'pg',
     connection: {
-        host : 'postgresql-slippery-80794',
-        user : 'postgres',
-        password : 'test',
-        database : 'smart-brain'
+        host: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
