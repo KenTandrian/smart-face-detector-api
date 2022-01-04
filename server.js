c83b9154ae29c4367af5d1696ddf504c2fb3b78e
KenTandrian <kenricktan11@gmail.com>
2022-01-04 19:19:02 +0700

    Fix port and nodemon

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
 app.put('/image', (req, res) => image.handleImage(req, res, db));
 app.post('/imageurl', (req, res) => image.handleApiCall(req, res));
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 });
 
 // const PORT = process.env.PORT;
